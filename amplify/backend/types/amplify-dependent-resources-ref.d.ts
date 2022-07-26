4f2289df1b6687bc3ca8636760483aa230017e59
sandip <sandip.chaudhari@motherson.com>
2022-07-26 10:09:12 +0530

    backedn added

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,29 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "nodejsdemoc49a67e7": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        },
+        "nodejsdemocdb2413c": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        },
+        "nodejsdemo25e10e0f": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "nextjsdemoapi": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
